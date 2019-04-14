e56896b535e4bb4b3e7b26c05b99af82aab92fab
Georgios Karamanis <>
2019-04-14 15:36:13 +0200

    noice

--- week 15/tennis2.R
+++ week 15/tennis2.R
@@ -1,13 +1,60 @@
 library(tidyverse)
-library(cheese)
+library(wesanderson)
 
-player_dob <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2019/2019-04-09/player_dob.csv")
-grand_slams <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2019/2019-04-09/grand_slams.csv")
 grand_slam_timeline <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2019/2019-04-09/grand_slam_timeline.csv")
 
-almostThere <- grand_slam_timeline %>%
+# WINS/LOSSES ONLY IN FINALS!
+winRatio <- as.tibble(grand_slam_timeline) %>%
   filter(., outcome == "Finalist" | outcome == "Won") %>% 
-  group_by(player, outcome) %>% count()
+  group_by(player) %>%
+  transmute(winner = sum(outcome == "Won"),
+            finalist = sum(outcome == "Finalist"),
+            ratio = winner / (winner + finalist),
+            total = winner + finalist,
+            hjust = if_else(winner > finalist, 0, 1),
+            yjust = if_else(winner > finalist, 0.1, -0.1)) %>% 
+  ungroup() %>% 
+  unique %>%
+  top_n(., 20, total) %>%
+  arrange(., desc(total))
 
-ggplot(notThere) +
-  geom_point(aes(year, player))
+winRatio$player <- reorder(winRatio$player, winRatio$total)
+winRatio$ratio <- round(winRatio$ratio*100, digits=0)
+
+pal <- wes_palette("BottleRocket2")
+
+ggplot(winRatio, aes(x = player)) +
+  geom_bar(aes(y = winner), stat = "identity", fill = pal[4]) +
+  geom_bar(aes(y = -finalist), stat = "identity", fill = pal[2]) +
+  annotate("text",
+           x = winRatio$player,
+           y = winRatio$winner - 0.7,
+           label = winRatio$winner,
+           size = 1,
+           color = "white",
+           family = "IBM Plex Mono",
+           hjust = 0) +
+  annotate("text",
+           x = winRatio$player,
+           y = -winRatio$finalist + 0.7,
+           label = winRatio$finalist,
+           size = 1,
+           color = "white",
+           family = "IBM Plex Mono",
+           hjust = 1) +
+  annotate("text",
+           x = winRatio$player,
+           y = winRatio$yjust,
+           label = winRatio$player,
+           size = 1,
+           color = "white",
+           family = "IBM Plex Mono",
+           fontface = "bold",
+           hjust = winRatio$hjust) +
+  coord_flip(ylim = c(-25, 25)) +
+  theme_void() +
+  theme(
+    panel.background = element_rect(fill = pal[3])
+  )
+
+ggsave("./week 15/tennis2.png", dpi = 600, height = 2, width = 6)
