32720ff902f281da9a7bc60422b9e1b8f8a6094e
Georgios Karamanis <georgios.karamanis@me.com>
2019-04-23 22:20:56 +0200

    final plot

--- week 17/anime.R
+++ week 17/anime.R
@@ -1,21 +1,18 @@
 library(tidyverse)
-library(RColorBrewer)
 
 tidy_anime <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2019/2019-04-23/tidy_anime.csv")
-
 # keep ID, names, genre and rating
-tidy_anime <- select(tidy_anime, c(1, 2, 3, genre, rating))
+tidyAnime <- select(tidy_anime, c(1, 2, 3, genre, rating))
 
 # Order ratings to use in y axis 
-ratLevels <- factor(unique(tidy_anime$rating))
+ratLevels <- factor(unique(tidyAnime$rating))
 ratLevels <- factor(ratLevels, levels(ratLevels)[c(1, 3, 4, 6, 5)])
 
-tidy_anime %>%
+tidyAnime %>%
   drop_na() %>% #Drop NA genre
   filter(rating != "None") %>%  #Drop "None" genre
   ggplot(aes(rating, genre)) +
   stat_bin2d(aes(fill = stat(count))) + 
   scale_x_discrete(limits = levels(ratLevels)) +
-  theme_minimal() +
   theme(
     )
