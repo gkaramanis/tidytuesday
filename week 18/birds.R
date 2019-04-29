ac3facaf67fdb5528fe37bf9f12d9e86dc698341
Georgios Karamanis <georgios.karamanis@me.com>
2019-04-29 21:55:16 +0200

    new week

---
+++ week 18/birds.R
@@ -0,0 +1,9 @@
+library(tidyverse)
+
+bird_collisions <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2019/2019-04-30/bird_collisions.csv")
+mp_light <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2019/2019-04-30/mp_light.csv")
+
+bird_collisions %>%
+  ggplot() +
+  geom_point(aes(date, genus)) +
+  scale_y_reverse()
