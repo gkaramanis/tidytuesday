db9a22b979faf35341f7e144c79731555616e623
Georgios Karamanis <georgios.karamanis@me.com>
2019-04-29 21:55:06 +0200

    some more exploring and trying but no good plot...

--- week 17/anime2.R
+++ week 17/anime2.R
@@ -6,7 +6,7 @@ tidy_anime <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience
 # remove NA
 tidyA <- tidy_anime %>%
   # set end_date to today for titles still airing and NA as end_date
-  mutate(end_date = replace(end_date, which(airing & is.na(end_date)), Sys.Date())) %>% 
+  # mutate(end_date = replace(end_date, which(airing & is.na(end_date)), Sys.Date())) %>% 
   # remove titles with NA end_date, remove movies and music
   filter(!is.na(end_date), type != "Movie", type != "Music") %>% 
   select(animeID, name, type, episodes, start_date, end_date) %>% 
@@ -24,16 +24,17 @@ tidyA %>%
                 ymin = startDateAsNum - 30, ymax = startDateAsNum + 30,
                 fill = as.numeric(diff_days), alpha = 0.5)) +
   scale_y_reverse() +
+  scale_x_date(limits = c(as.Date("1960-01-01"), Sys.Date()), date_breaks = "10 years", date_labels = "%Y") +
   theme_bw() +
   scale_fill_gradientn(colours = pal) +
   theme(
     panel.background = element_rect(fill = "gray90"),
-    # axis.text.y = element_blank(),
-    # axis.ticks.y = element_blank(),
+    axis.text.y = element_blank(),
+    axis.ticks.y = element_blank(),
     panel.border = element_blank(),
     # panel.grid = element_blank(),
-    # legend.position="none",
+    legend.position="none",
     text = element_text(family = "IBM Plex Sans", size = 6)
   ) +
-  ggsave("./week 17/anime2.png", dpi = 600, height = 6, width = 4)
+  ggsave("/Users/Georgios/Documents/Projects/tidytuesday/week 17/anime2.png", dpi = 600, height = 6, width = 4)
 
