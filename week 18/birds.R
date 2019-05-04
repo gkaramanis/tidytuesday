564c39719c88a7417e9a248a5992fd3407c916ff
Georgios Karamanis <>
2019-05-04 13:41:31 +0200

    new week

--- week 18/birds.R
+++ week 18/birds.R
@@ -1,5 +1,5 @@
 library(tidyverse)
-library(lubridate)
+library(zoo)
 library(wesanderson)
 
 bird_collisions <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2019/2019-04-30/bird_collisions.csv")
@@ -7,12 +7,20 @@ bird_collisions <- readr::read_csv("https://raw.githubusercontent.com/rfordatasc
 pal <- wes_palette("Zissou1", 10, type = "continuous")
 
 # my week 12 "policing.R" for inspiration
-bird_collisions %>%
-  ggplot(aes(date, reorder(genus, desc(genus)))) +
-  geom_point(aes(alpha = 0.1, colour = factor(species))) +
+tallyB <- bird_collisions %>%
+  select(genus, date) %>% 
+  mutate(yearMon = as.yearmon(bird_collisions$date, "%Y-%m")
+) %>%
+  mutate(mon = format(yearMon, "%m")) %>% 
+  group_by(genus, yearMon, mon) %>% 
+  summarise(n=n())
+
+ggplot(tallyB, aes(yearMon, reorder(genus, desc(genus)))) +
+  geom_point(aes(alpha = 0.1, size = factor(yearMon))) +
   scale_fill_gradientn(colours = pal) +
-  theme_dark() +
+  # theme_dark() +
   theme(
     legend.position = "none",
-    panel.background = element_rect(fill = "gray20")
+    # panel.background = element_rect(fill = "gray20")
   )
+
