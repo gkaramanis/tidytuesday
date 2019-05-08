f7ac3335a663f02ddc99cf3ec34403b364eb84ce
Georgios Karamanis <georgios.karamanis@me.com>
2019-05-08 08:34:36 +0200

    working

--- week-19/students.R
+++ week-19/students.R
@@ -2,6 +2,7 @@ library(tidyverse)
 
 student_ratio <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2019/2019-05-07/student_teacher_ratio.csv")
 
-ggplot(student_ratio, aes(student_ratio, indicator)) +
+ggplot(student_ratio, aes(student_ratio, factor(indicator))) +
   geom_jitter(height = 0.1, alpha = 0.2) +
+  scale_y_discrete(labels="edulit_ind") +
   theme_minimal()
