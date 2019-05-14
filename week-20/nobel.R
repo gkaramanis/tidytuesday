8f773b2a33645ba6451eef36d6434afd08555b61
Georgios Karamanis <>
2019-05-14 15:30:33 +0200

    arrows!

--- week-20/nobel.R
+++ week-20/nobel.R
@@ -4,26 +4,30 @@ nobel_winners <- readr::read_csv("https://raw.githubusercontent.com/rfordatascie
 
 nobel_winners %>%
   filter(!is.na(death_country)) %>%
-  # filter(birth_country != death_country) %>% 
-  ggplot(aes(fct_rev(factor(birth_country)), x = 0,
-         xend = death_country, yend = 0)) +
+  filter(birth_country != death_country) %>% 
+  ggplot(aes(y = fct_rev(factor(birth_country)), x = 0,
+         xend = death_country, yend = 1)) +
   # geom_curve(data=subset(nobel_winners, birth_country > death_country), 
              # curvature = 1, alpha = 0.3) +
-  geom_curve(curvature = -0.4, alpha = 0.1) +
+  geom_curve(
+    curvature = -0.5,
+    alpha = 0.1,
+    arrow = arrow(length = unit(0.01, "npc"))
+    ) +
   scale_x_discrete() +
   scale_y_discrete() +
   # facet_grid(category ~ .) +
   theme_minimal() +
   theme(
-    # axis.text.y = element_blank(),
-    axis.text.x = element_text(angle = 90, hjust = 1)
+    axis.text.x = element_text(angle = 40, hjust = 1),
+    panel.grid = element_blank()
   )
 
 
 nobel_winners %>% 
   filter(!is.na(death_country)) %>% 
   mutate(diffCountry = ifelse(birth_country == death_country, 0, 1)) %>% 
-  group_by(category) %>% 
+  group_by(death_country) %>% 
   tally(diffCountry) %>% 
   arrange(desc(n))
          
