424022d49019ada191c5e7eabe532bb96d2dd830
Georgios Karamanis <georgios.karamanis@me.com>
2019-05-14 15:08:55 +0200

    noice!

--- week-20/nobel.R
+++ week-20/nobel.R
@@ -4,14 +4,21 @@ nobel_winners <- readr::read_csv("https://raw.githubusercontent.com/rfordatascie
 
 nobel_winners %>%
   filter(!is.na(death_country)) %>%
-  ggplot() +
-  geom_segment(aes(x = 1, y = birth_country,
-                 xend = 2, yend = death_country),
-               alpha = 0.3) +
-  facet_grid(. ~ category) +
+  # filter(birth_country != death_country) %>% 
+  ggplot(aes(fct_rev(factor(birth_country)), x = 0,
+         xend = death_country, yend = 0)) +
+  # geom_curve(data=subset(nobel_winners, birth_country > death_country), 
+             # curvature = 1, alpha = 0.3) +
+  geom_curve(curvature = -0.4, alpha = 0.1) +
+  scale_x_discrete() +
+  scale_y_discrete() +
+  # facet_grid(category ~ .) +
+  theme_minimal() +
   theme(
-    axis.text = element_blank()
+    # axis.text.y = element_blank(),
+    axis.text.x = element_text(angle = 90, hjust = 1)
   )
+
 
 nobel_winners %>% 
   filter(!is.na(death_country)) %>% 
