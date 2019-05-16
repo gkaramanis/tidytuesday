64ee851ff461c70deacd5ca165fee3abd518a3e6
Georgios Karamanis <georgios.karamanis@me.com>
2019-05-16 22:33:54 +0200

    noice!

--- week-20/nobelMap.R
+++ week-20/nobelMap.R
@@ -9,7 +9,7 @@ nobelCountries <- nobel_winners %>%
   select(c("birth_country", "death_country")) %>% 
   # remove NA death countries
   filter(!is.na(death_country)) %>%
-  # Clean birth countries
+  # Clean birth and death countries (to get current countries in map) 
   mutate(birth_con = str_extract(birth_country, "\\(([^()]*)\\)")) %>%
   mutate(birth_country = 
            if_else(
@@ -26,6 +26,22 @@ nobelCountries <- nobel_winners %>%
            )
          
   ) %>% 
+  mutate_at(vars(birth_country, death_country),
+            ~ replace(., which(.== "Czechoslovakia"), "Czech Republic")) %>%
+  mutate_at(vars(birth_country, death_country),
+            ~ replace(., which(.== "Northern Ireland"), "United Kingdom")) %>%
+  mutate_at(vars(birth_country, death_country),
+            ~ replace(., which(.== "Scotland"), "United Kingdom")) %>%
+  mutate_at(vars(birth_country, death_country),
+            ~ replace(., which(.== "East Germany"), "Germany")) %>%
+  mutate_at(vars(birth_country, death_country),
+            ~ replace(., which(.== "Republic of Macedonia"), "Macedonia")) %>%
+  mutate_at(vars(birth_country, death_country),
+            ~ replace(., which(.== "Serbia"), "Republic of Serbia")) %>%
+  mutate_at(vars(birth_country, death_country),
+            ~ replace(., which(.== "Guadeloupe Island"), "France")) %>%
+  mutate_at(vars(birth_country, death_country),
+            ~ replace(., which(.== "Union of Soviet Socialist Republics"), "Russia")) %>%
   mutate(
     colour = case_when(
       death_country == "United States of America" ~ "#FF2B4F",
@@ -40,17 +56,39 @@ nobelCountries <- nobel_winners %>%
 # keep only entries with different birth and death country
 filter(birth_country != death_country)
 
-countriesLL <- countriesCoord %>%  select(c("name_long", "Longitude", "Latitude"))
+countriesLL <- countriesCoord %>% select(c("admin", "Longitude", "Latitude"))
 
-nobelLL <- merge(nobelCountries, countriesLL, by.x = "birth_country", by.y = "name_long")
+nobelLL <- merge(nobelCountries, countriesLL, by.x = "birth_country", by.y = "admin", all.x = TRUE)
 nobelLL <- nobelLL %>% rename(fromLong = Longitude, fromLat = Latitude) 
-nobelLL <- merge(nobelLL, countriesLL, by.x = "death_country", by.y = "name_long")
+nobelLL <- merge(nobelLL, countriesLL, by.x = "death_country", by.y = "admin", all.x = TRUE)
 nobelLL <- nobelLL %>% rename(toLong = Longitude, toLat = Latitude) 
 
-ggplot(nobelLL) +
-  geom_curve(aes(x = fromLat, y = fromLong, xend = toLat, yend = toLong))
+worldmap <- borders("world", colour = "gray60", fill = "gray80", size = 0.05)
 
-qmplot(data = nobelLL, x = fromLong, y = fromLat, xend = toLong, yend = toLat, maptype = "toner-lite", geom = "curve") +
-coord_cartesian() +
-ggsave("./week-20/nobelMap.png", width = 5, height = 3)
+ggplot(nobelLL, aes(
+  x = fromLong, y = fromLat,
+  xend = toLong, yend = toLat,
+  colour = colour,
+  alpha = (colour != "gray60"))) +
+  scale_color_identity() +
+  scale_alpha_manual(values = c(0.3, 0.5), guide = FALSE) +
+  scale_size_manual(values = c(0.05, 0.1), guide = FALSE) +
+  labs(title = "Birth and death countries of Nobel laureates that were born and died in different countries",
+       subtitle = "Arrow heads show the death country (USA, W. Germany, UK, Germany, France and Switzerland are the most common)", 
+       caption = "\nSource: Kaggle | Graphic: Georgios Karamanis / @geokaramanis") +
+  worldmap +
+  coord_cartesian(ylim = c(-50, 105)) +
+  geom_curve(aes(x = fromLong, y = fromLat,
+                 xend = toLong, yend = toLat,
+                 size = colour != "gray60"),
+             arrow = arrow(length = unit(0.009, "npc"))) +
+  theme_void() +
+  theme(
+    plot.background = element_rect(fill = "#F0EFF1", colour = "#F0EFF1"),
+    text = element_text(family = "IBM Plex Sans", size = 5),
+    plot.title = element_text(face = "bold"),
+    plot.subtitle = element_text(vjust = 2),
+  ) +
+  ggsave("./week-20/nobelMap.png", width = 6, height = 3.5)
 
+nobelLL %>% filter()
