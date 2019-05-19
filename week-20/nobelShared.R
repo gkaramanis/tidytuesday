c89f6a2643700f0f3c7db1e5641e94d3919c32e4
Georgios Karamanis <georgios.karamanis@me.com>
2019-05-19 13:26:22 +0200

    noice!

--- week-20/nobelShared.R
+++ week-20/nobelShared.R
@@ -3,28 +3,30 @@ library(tidyverse)
 nobel_winners <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2019/2019-05-14/nobel_winners.csv")
 
 nobel_winners %>%
+  filter(category == "Medicine") %>% 
   mutate(nume = as.numeric(str_sub(prize_share, 1, 1)),
          deno = as.numeric(str_sub(prize_share, -1)),
          share = nume/deno,
          year = prize_year %% 10,
-         decade = prize_year - 1900 - year) %>% 
+         decade = prize_year - 1900 - year) %>%
+  group_by(prize_year) %>% 
+  distinct(full_name, .keep_all = TRUE) %>% 
+  mutate(n = row_number()) %>% 
   ggplot() + 
-  geom_bar(
-    aes(x = "", y = share),
+  geom_bar(aes(x = "", y = share, fill = n),
     stat = "identity", show.legend = FALSE
   ) +
-  # coord_polar("y", start = 0) +
-  facet_grid(decade ~ year, switch = "y") 
-  # scale_x_continuous(breaks = seq(0, 9, by = 1)) +
-  # scale_y_reverse(labels = seq(1900, 2020, by = 10),
-  #                 breaks = seq(0, 120, by = 10))
-
-ggplot(z, aes(x=prize_year, fill = deno)) +
-  geom_bar(aes(x = prize_year, y = share),
-           stat = "identity", show.legend = FALSE) +
-  facet_grid(category ~ .)
-  
-  
-  
-  
-
+  scale_fill_identity() +
+  coord_polar("y") +
+  facet_grid(decade ~ year) +
+  labs(title = "Shared Nobel Prizes in Medicine",
+       subtitle = "Cumulative lethal collisions, 1980-2017",
+       caption = "\nSource: Kaggle | Graphic: Georgios Karamanis / @geokaramanis") +
+  theme_void() +
+  theme(
+    plot.background = element_rect(fill = "#F0EFF1", colour = "#F0EFF1"),
+    plot.margin = unit(c(0.2, 0.2, 0.2, 0.6), "cm"),
+    text = element_text(family = "IBM Plex Sans", size = 6),
+    plot.title = element_text(face = "bold"),
+    plot.subtitle = element_text(vjust = 2)
+  ) 
