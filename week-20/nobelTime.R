bce7d5a5f37bfb91df1b6825d41909a3391373bf
Georgios Karamanis <georgios.karamanis@me.com>
2019-05-19 08:13:40 +0200

    trying new things

--- week-20/nobelTime.R
+++ week-20/nobelTime.R
@@ -2,6 +2,7 @@ library(tidyverse)
 
 nobel_winner_all_pubs <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2019/2019-05-14/nobel_winner_all_pubs.csv")
 
+# first try, but there are multiple papers for some authors ----
 nobel_winner_all_pubs %>%
   filter(is_prize_winning_paper == "YES") %>% 
   mutate(yearsToPrize = prize_year - pub_year) %>% 
@@ -12,3 +13,11 @@ nobel_winner_all_pubs %>%
   # geom_point(aes(x = prize_year, y = id)) +
   geom_rect(aes(xmin = pub_year, xmax = prize_year,
                 ymin = id, ymax = id + 1))
+
+# find unique combinations, but losing papers ----
+winP <- nobel_winner_all_pubs %>%
+  filter(is_prize_winning_paper == "YES") %>% 
+  mutate(yearsToPrize = prize_year - pub_year) %>% 
+  arrange(desc(pub_year)) %>%
+  distinct(laureate_id, prize_year, .keep_all = TRUE) %>% 
+  mutate(id = row_number())
