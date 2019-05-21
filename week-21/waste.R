19da01f82c29fc6d8ebae3761cd8bffa52131fae
Georgios Karamanis <georgios.karamanis@me.com>
2019-05-21 05:45:11 +0200

    starting to look nice

--- week-21/waste.R
+++ week-21/waste.R
@@ -1,3 +1,22 @@
 library(tidyverse)
-library(waffle)
+library(ggimage)
+# library(rsvg)
 library(janitor)
+
+coast_vs_waste <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2019/2019-05-21/coastal-population-vs-mismanaged-plastic.csv")
+mismanaged_vs_gdp <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2019/2019-05-21/per-capita-mismanaged-plastic-waste-vs-gdp-per-capita.csv")
+
+coast_vs_waste <- clean_names(coast_vs_waste)
+mismanaged_vs_gdp <- clean_names(mismanaged_vs_gdp)
+
+mismanaged_vs_gdp %>% 
+  filter(entity != "World" & year == 2010) %>% 
+  top_n(20, per_capita_mismanaged_plastic_waste_kilograms_per_person_per_day) %>% 
+  mutate(plastic = sample(c("https://image.flaticon.com/icons/png/128/81/81940.png",
+                           "https://image.flaticon.com/icons/png/128/1758/1758890.png",
+                           "https://image.flaticon.com/icons/png/128/960/960773.png"),
+                          size =  nrow(.), replace = TRUE)) %>% 
+ggplot(aes(fct_rev(factor(entity)), per_capita_mismanaged_plastic_waste_kilograms_per_person_per_day)) +
+  geom_image(aes(image = plastic), color = "blue", size=0.03) +
+  coord_flip() +
+  theme_minimal()
