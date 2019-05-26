e1fe01a504412ae7439d6fa24c43886dcd5eba22
Georgios Karamanis <georgios.karamanis@me.com>
2019-05-26 21:54:27 +0200

    trying something new

--- week-21/waste2.R
+++ week-21/waste2.R
@@ -9,13 +9,16 @@ mismanaged_vs_gdp %>%
   rename(percapita_mismanaged_kg_pp_pd = per_capita_mismanaged_plastic_waste_kilograms_per_person_per_day) %>% 
   filter(entity != "World" & year == 2010) %>% 
   top_n(30, percapita_mismanaged_kg_pp_pd) %>% 
+  mutate(g_pp_pd = percapita_mismanaged_kg_pp_pd * 1000) %>% 
   mutate(plastic = sample(c("https://image.flaticon.com/icons/png/128/81/81940.png",
                             "https://image.flaticon.com/icons/png/128/1758/1758890.png",
                             "https://image.flaticon.com/icons/png/128/960/960773.png"),
                           size =  nrow(.), replace = TRUE)) %>% 
   ggplot(aes(x = gdp_per_capita_ppp_constant_2011_international_rate,
              y = total_population_gapminder,
-             size = percapita_mismanaged_kg_pp_pd)) +
+             size = g_pp_pd)) +
   geom_point() +
+  geom_image(aes(image = plastic), color = "royalblue1", size = 0.04) +
   scale_y_log10()
-  
+
+ggsave("./week-21/waste2.png")
