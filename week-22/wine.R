a3ce6a14b7cff8414e0d06ac0261bbeb5ceb37c0
Georgios Karamanis <georgios.karamanis@me.com>
2019-05-29 05:37:31 +0200

    new week

---
+++ week-22/wine.R
@@ -0,0 +1,30 @@
+library(tidyverse)
+library(ggrepel)
+library(tidytext)
+
+wine_ratings <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2019/2019-05-28/winemag-data-130k-v2.csv")
+
+wineWords <- wine_ratings %>% 
+  select(-c("X1")) %>% 
+  distinct() %>% 
+  # group_by(taster_name) %>% 
+  # filter(!is.na(taster_name)) %>% 
+  select(taster_name, description) %>%
+  unnest_tokens(word, description) %>%    
+  anti_join(stop_words)
+
+wineWords %>%
+  filter(!is.na(taster_name)) %>% 
+  group_by(taster_name, word) %>%
+  summarise(n = n()) %>% 
+  mutate(freq = n / sum(n)) %>% 
+  arrange(desc(freq)) %>%
+  group_by(taster_name) %>%
+  slice(seq_len(3)) %>% 
+  ggplot() +
+  geom_text(aes(label = word, color = word,
+                 x = freq, y = taster_name)) +
+  theme_minimal()
+  
+  
+  
