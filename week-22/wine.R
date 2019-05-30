00202dfc067d68f5d06b73bb8e82542943e93a33
Georgios Karamanis <georgios.karamanis@me.com>
2019-05-30 13:51:09 +0200

    quit this one

--- week-22/wine.R
+++ week-22/wine.R
@@ -1,33 +1,54 @@
 library(tidyverse)
-library(tidytext)
-library(ggrepel)
 
 wine_ratings <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2019/2019-05-28/winemag-data-130k-v2.csv")
 
-wineWords <- wine_ratings %>% 
-  select(-c("X1")) %>% 
-  distinct() %>% 
-  # group_by(taster_name) %>% 
-  # filter(!is.na(taster_name)) %>% 
-  select(taster_name, description) %>%
-  unnest_tokens(word, description) %>%    
-  anti_join(stop_words)
-
-wineWords %>%
-  filter(!is.na(taster_name)) %>% 
-  group_by(taster_name, word) %>%
-  summarise(n = n()) %>% 
-  mutate(freq = n / sum(n)) %>% 
-  arrange(desc(freq)) %>%
-  group_by(taster_name) %>%
-  slice(seq_len(3)) %>% 
-  ggplot() +
-  geom_text_repel(aes(label = word, color = word,
-                 x = freq, y = taster_name)) +
-  theme_minimal() +
-  theme(
-    text = element_text(family = "IBM Plex Sans", size = 8)
+food <- tribble(
+   ~type, ~examples,
+  "cured meat",    "salami",
+  "pasta",     "pasta",
+  "pasta",     "spaghetti",
+  "pasta",     "lasagna",
+  "mushroom",  "mushroom",
+  "meat",      "meat",
+  "meat",      "sausage",
+  "meat",      "beef",
+  "meat",      "hamburger",
+  "meat",      "pork",
+  "meat",      "veal",
+  "meat",      "lamb",
+  "desserts",  "desserts",
+  "seafood",   "seafood",
+  "seafood",      "fish",
+  "seafood",    "shrimp",
+  "seafood",     "clams",
+  "seafood",    "oyster",
+  "seafood",   "lobster",
+  "cheese",    "cheese",
+  "cheese",    "mozzarella",
+  "poultry",   "poultry",
+  "poultry",   "chicken",
+  "vegetables", "vegetable",
+  "vegetables", "tomato",
+  "vegetables", "eggplant",
+  "vegetables",     "salad",
+  "pizza", "pizza" 
   )
 
-ggsave("wine.png")  
+pairings <- wine_ratings %>% 
+  select(-c("X1")) %>% 
+  distinct() %>% 
+  select("variety", "description") %>% 
+  filter(str_detect(description, "pair\\w*\\b with")) %>%
+  mutate(pairWith = str_extract(description,
+                                "pair\\w*\\b with(?:(?=[\\s.?!])[^.?!]*(?:[.?!].*)?)\\.")) %>% 
+  mutate(found = str_extract_all(pairWith,
+                                 paste(food$examples,collapse="|")))
+
+# at the end
+counted <- pairings %>%
+  count(variety, sort = TRUE) %>% 
+  top_n(5)
+
+# Corvina, Rondinella, Molinara -> Valpolicella
+# write.csv(pairings$description, file = "winePairings.csv")
   
