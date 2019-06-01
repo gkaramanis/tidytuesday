dfc8ea832dbfba95ba1045f58931de8b4784275b
Georgios Karamanis <georgios.karamanis@me.com>
2019-06-01 22:31:32 +0200

    give up!

--- week-22/wine.R
+++ week-22/wine.R
@@ -6,34 +6,42 @@ wine_ratings <- readr::read_csv("https://raw.githubusercontent.com/rfordatascien
 
 food <- tribble(
    ~type, ~example,
-  "cured meat",    "salami",
-  "pasta",     "pasta",
-  "pasta",     "spaghetti",
-  "pasta",     "lasagna",
-  "mushroom",  "mushroom",
-  "meat",      "meat",
-  "meat",      "sausage",
-  "meat",      "beef",
-  "meat",      "hamburger",
-  "meat",      "pork",
-  "meat",      "veal",
-  "meat",      "lamb",
+  "meat &\ncured meat",    "salami",
+  "pasta &\npizza",     "pasta",
+  "pasta &\npizza",     "spaghetti",
+  "pasta &\npizza",     "lasagna",
+  "pasta &\npizza",     "ravioli",
+  "vegetables &\nmushrooms",  "mushroom",
+  "meat &\ncured meat",      "meat",
+  "meat &\ncured meat",      "sausage",
+  "meat &\ncured meat",      "beef",
+  "meat &\ncured meat",      "hamburger",
+  "meat &\ncured meat",      "pork",
+  "meat &\ncured meat",      "veal",
+  "meat &\ncured meat",      "lamb",
+  "meat &\ncured meat",      "ribs",
   "dessert",  "dessert",
   "seafood",   "seafood",
+  "seafood",   "scallops",
+  "seafood",   "tuna",
+  "seafood",   "tilapia",
   "seafood",      "fish",
+  "seafood",      "salmon",
   "seafood",    "shrimp",
   "seafood",     "clams",
   "seafood",    "oyster",
   "seafood",   "lobster",
   "cheese",    "cheese",
   "cheese",    "mozzarella",
+  "cheese",    "pecorino",
   "poultry",   "poultry",
   "poultry",   "chicken",
-  "vegetables", "vegetable",
-  "vegetables", "tomato",
-  "vegetables", "eggplant",
-  "vegetables",     "salad",
-  "pizza", "pizza" 
+  "poultry",   "duck",
+  "vegetables &\nmushrooms", "vegetable",
+  "vegetables &\nmushrooms", "tomato",
+  "vegetables &\nmushrooms", "eggplant",
+  "vegetables &\nmushrooms",     "salad",
+  "pasta &\npizza", "pizza" 
   )
 
 pairings <- wine_ratings %>%
@@ -65,56 +73,74 @@ pairings <- wine_ratings %>%
   ungroup() %>% 
   # number type groups
   group_by(type) %>% 
-  mutate(typeNr = group_indices()) %>% 
+  mutate(typeNr = group_indices()*1.5-0.5) %>% 
   ungroup() %>% 
-  # images
-  mutate(bottleImg = "./week-22/img/bottle.png",
-         foodImg = paste("./week-22/img/", type, ".png", sep = ""),
-         bottleColor = ifelse(variety == "Chardonnay" | variety == "White Blend", "#5e1224", "#e3c979"))
+  # count pairings per variety and type
+  group_by(varietyNr, typeNr) %>% 
+  mutate(ntype = n()) %>% 
+  ungroup() %>% 
+  # needed for images
+  rowwise() %>%
+  # images and colors
+  mutate(foodImg = paste("./week-22/img/", substr(type, 1, 4), ".png", sep = ""),
+         bottleColor = ifelse(variety == "Chardonnay" | variety == "White Blend", "#D4C52D", "#5e1224"),
+         nudge = runif(1)/5 - 0.1)
 
-  
+# plot pairings
 ggplot() +
-  
-  geom_diagonal(data=pairings,
-             aes(x = varietyNr, y = 10,
-                 xend = typeNr, yend = 1,
-                 color = varietyNr),
-             curvature = -0.1,
-             size = 0.2, 
-             alpha = 0.4) +
-  
-  geom_image(aes(image = unique(pairings$bottleImg), 
-                 x = unique(pairings$varietyNr), 
-                 y = 11.6)) +
-  
-  geom_image(aes(image = unique(pairings$foodImg), 
-                 x = unique(pairings$typeNr), 
-                 y = 0.4), size = 0.05) +
-  
+  # down
+  geom_segment(data = pairings,
+             aes(x = varietyNr + nudge, y = 10,
+                 xend = typeNr + nudge, yend = 1,
+                 colour = bottleColor),
+             size = pairings$ntype/30,
+             lineend = "butt",
+             alpha = 0.2) +
+  # bottle icons
+  geom_image(aes(image = "./week-22/img/bottle.png",
+  # color = "#D4C52D",
+                 x = c(1.5, 9.5),
+                 y = 11.3)) +
+  geom_image(aes(image = "./week-22/img/bottle.png",
+  # color = "#5e1224",
+                 x = c(3.5, 5.5, 7.5),
+                 y = 11.3)) +                
+  # food icons
+  geom_image(aes(image = unique(pairings$foodImg),
+                 x = unique(pairings$typeNr),
+                 y = 0.3), size = 0.05) +
+  # variety and food names
   geom_text(aes(label = unique(pairings$variety),
-                x = unique(pairings$varietyNr), y = 10.4),
-            size = 2) +
-  
+                x = unique(pairings$varietyNr),
+                y = 12.8),
+            size = 1.8,
+            family = "IBM Plex Serif") +
+  geom_text(aes(label = unique(pairings$type),
+                x = unique(pairings$typeNr),
+                y = -0.4),
+            size = 1.4,
+            vjust = 1,
+            family = "IBM Plex Serif") +
+
   expand_limits(y = c(1, 13)) +
   scale_color_identity() +
-  
-  labs(title = "Top 5 wine varieties paired with 9 food types",
-       subtitle = "as mentioned by tasters in reviews",
-       caption = "Source: XXX | Graphic: Georgios Karamanis") +
+
+  labs(title = "Wine and food pairings for the top 5 varieties",
+       subtitle = "as recommended by WineEnthusiast reviewers",
+       caption = "Source: kaggle.com | Graphic: Georgios Karamanis") +
   
   theme_void() +
   theme(
     legend.position =  "",
-    plot.background = element_rect(fill = "#ebd9a0", colour = "lightgoldenrod3"),
+    plot.background = element_rect(fill = "#F0EFF1", colour = "#F0EFF1"),
     plot.margin = unit(c(1, 1, 1, 1), "cm"),
-    text = element_text(family = "IBM Plex Sans", size = 8),
+    text = element_text(family = "IBM Plex Serif", size = 8),
     plot.title = element_text(face = "bold", vjust = 8),
     plot.subtitle = element_text(vjust = 9),
     plot.caption = element_text(size = 4, vjust = -3)
   )
 
-
 ggsave("./week-22/wine.png", height = 5, width = 4, dpi = 600)
 
-# write.csv(pairings$description, file = "winePairings.csv")
+# write.csv(pairings$pairWith, file = "winePairings.csv")
   
