41ab7c8aea2b7172f0a50bf95dac5b5af34749c1
Georgios Karamanis <georgios.karamanis@me.com>
2019-06-03 21:45:44 +0200

    you don't need ggforce

--- week-22/wine.R
+++ week-22/wine.R
@@ -1,6 +1,5 @@
 library(tidyverse)
 library(ggimage)
-library(ggforce)
 
 wine_ratings <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2019/2019-05-28/winemag-data-130k-v2.csv")
 
