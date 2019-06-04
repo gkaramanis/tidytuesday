4dcff67ce7be4ef93e35e059f3bce86009199d85
Georgios Karamanis <georgios.karamanis@me.com>
2019-06-04 20:24:08 +0200

    cooking something good

--- week-23/ramen.R
+++ week-23/ramen.R
@@ -7,8 +7,28 @@ shio <- ramen_ratings %>%
   filter(country == "Japan", str_detect(variety, "Shio")) %>% 
   mutate(n = 1:n())
 
+# japanese chopstics are pointy!
+
 ggplot(shio, aes(n, stars)) +
-  geom_col() +
-  scale_y_reverse(limits = (0, 7)) +
+  geom_segment(aes(x = -20, y = 0.12, 
+                   xend = 32, yend = 0.12),
+               size = 2.7, lineend = "round", color = "#612711") +
+  geom_segment(aes(x = -20, y = 0.12, 
+                   xend = 20, yend = 0.12),
+               size = 3, lineend = "square", color = "#612711") +
+  geom_segment(aes(x = -20, y = 0.4, 
+                   xend = 32, yend = 0.3),
+               size = 2.7, lineend = "round", color = "#612711") +
+  geom_segment(aes(x = -20, y = 0.4, 
+                   xend = 20, yend = 0.3),
+               size = 3, lineend = "square", color = "#612711") +
+  geom_col(width = 0.3, fill = "#DBD067") +
+  scale_y_reverse(limits = c(8, 0)) +
+  theme_minimal() +
   theme(
+    panel.background = element_rect(fill = "#0881A3")
   )
+
+ggsave("./week-23/ramen.png", height = 5, width = 5)
+
+
