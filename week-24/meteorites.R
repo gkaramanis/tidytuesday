b5436d89a8123270810d3e5acd3b60e2a304fd73
Georgios Karamanis <>
2019-06-12 16:56:06 +0200

    planet plot

--- week-24/meteorites.R
+++ week-24/meteorites.R
@@ -1,6 +1,5 @@
 library(tidyverse)
-library(sp)
-library(ggmap)
+#library(sp)
 library(rworldmap)
 
 meteorites <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2019/2019-06-11/meteorites.csv")
@@ -19,28 +18,39 @@ fellMeteo <- meteorites %>%
   drop_na() %>%
   filter(fall == "Fell") %>% 
   select(long, lat) %>% 
-  mutate(country = coords2continent(.)) %>% 
+  mutate(country = coords2continent(.)) %>%
+  mutate(country = str_replace_all(country, "United States of America", "U.S.A.")) %>% 
   drop_na() %>% 
   left_join(., meteorites) %>% 
   group_by(country) %>% 
-  mutate(medianMass = median(mass),
+  mutate(medianMass = median(mass)/1000,
          sumFell = sum(n()),
          countryLat = mean(lat),
          countryLong = mean(long)) %>% 
-  distinct(country, medianMass, sumFell, countryLat, countryLong)
+  filter(sumFell > 21) %>% 
+  mutate(countryNr = group_indices())
 
-worldmap <- borders("world", colour = "gray60", fill = "gray80", size = 0.05)
+
 
 fellMeteo %>% 
-  ggplot(aes(countryLong, countryLat)) + 
-  worldmap +
-  coord_cartesian(xlim = c(-170, 180), ylim = c(-55, 85)) +
-  geom_point(aes(size = medianMass), alpha = 0.5) +
-  geom_curve(aes(xend = countryLong - sumFell,
-                 yend = countryLat + sumFell), curvature = 0.2) +
-  theme_void() +
+  ggplot(aes(countryNr + lat/100, 3)) +
+  geom_segment(aes(xend = countryNr + lat/100,
+                   yend = 15 - sumFell/100,
+                   size = 1),
+               alpha = 0.5,
+               color = "orange") +
+  geom_point(aes(size = mass),
+             color = "orangered",
+             alpha = 0.5) +
+  theme_minimal() +
+  ylim(0, 15) +
+  coord_polar() +
   theme(
-    legend.position = "bottom"
-  )
+    axis.text.x = element_text(angle = 60, hjust = 1)
+  ) +
 
-ggsave("./week-24/meteorites.png", width = 5, height = 3)
+ggsave("./week-24/meteorites.png")
+
+
+
+
