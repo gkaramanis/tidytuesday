1202979a04dac74505ca895aba2839bb795a0ea4
Georgios Karamanis <>
2019-06-24 20:39:09 +0200

    new week

---
+++ week-26/README.md
@@ -0,0 +1,2 @@
+https://github.com/rfordatascience/tidytuesday/tree/master/data/2019/2019-06-25
+![](ufo.png)
---
+++ week-26/ufo.R
@@ -0,0 +1,4 @@
+library(here)
+library(tidyverse)
+
+ufo_sightings <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2019/2019-06-25/ufo_sightings.csv")
