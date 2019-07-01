a177d8abd9b7f4351f5ce9feb1ff5ed812a5c414
Georgios Karamanis <>
2019-07-01 20:13:48 +0200

    new week

---
+++ week-27/README.md
@@ -0,0 +1,3 @@
+![](wip_sorry.jpg)
+https://github.com/rfordatascience/tidytuesday/tree/master/data/2019/2019-07-02
+![](media_franchises.png)
---
+++ week-27/media_franchises.R
@@ -0,0 +1,3 @@
+library(tidyverse)
+
+media_franchises <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2019/2019-07-02/media_franchises.csv")
