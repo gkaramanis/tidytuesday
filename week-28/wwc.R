fe37642ba5546cd32acbd902e6436307773a4a31
Georgios Karamanis <georgios.karamanis@me.com>
2019-07-09 22:15:56 +0200

    kick-off!

---
+++ week-28/wwc.R
@@ -0,0 +1,10 @@
+library(tidyverse)
+
+wwc_outcomes <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2019/2019-07-09/wwc_outcomes.csv")
+squads <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2019/2019-07-09/squads.csv")
+codes <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2019/2019-07-09/codes.csv")
+
+wwc_outcomes %>% group_by(team) %>% 
+  ggplot() +
+  geom_point(aes(year, win_status)) +
+  facet_grid(team~.)
