60d24f19fa5c89c17428da6c2fa86ee18ccf070e
Georgios Karamanis <georgios.karamanis@me.com>
2019-07-10 21:08:49 +0200

    I'm defeating the opponent

--- week-28/README.md
+++ week-28/README.md
@@ -1,3 +1,4 @@
 https://github.com/rfordatascience/tidytuesday/tree/master/data/2019/2019-07-09
 
 ![](wip_sorry.jpg)
+![](/week-28/wwc.png)
--- week-28/wwc.R
+++ week-28/wwc.R
@@ -1,10 +1,37 @@
 library(tidyverse)
+library(here)
 
 wwc_outcomes <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2019/2019-07-09/wwc_outcomes.csv")
-squads <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2019/2019-07-09/squads.csv")
 codes <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2019/2019-07-09/codes.csv")
 
-wwc_outcomes %>% group_by(team) %>% 
-  ggplot() +
-  geom_point(aes(year, win_status)) +
-  facet_grid(team~.)
+winloss <- wwc_outcomes %>%
+  group_by(team) %>%
+  mutate(
+    game_n = row_number(),
+    win_i = -2 + as.integer(factor(win_status))
+   )
+  
+ggplot(winloss) +
+  geom_tile(aes(x = 26, y = 0), height = 3, 
+            width = 52, fill = "grey90") +
+  geom_tile(aes(x = game_n, y = win_i), color = "white") +
+  geom_text(aes(label = team, x = -1, y = 0),
+    hjust = 1, size = 3, check_overlap = TRUE,
+    family = "IBM Plex Mono") +
+  coord_fixed(xlim = c(-2, 50)) +
+  facet_wrap(~ team, ncol = 2) +
+  theme_minimal() +
+  theme(
+    legend.position = "none",
+    panel.grid = element_blank(),
+    axis.text.y = element_blank(),
+    axis.title = element_blank(),
+    strip.text = element_blank()
+  ) +
+  ggsave(here("week-28", "wwc.png"))
+  
+  
+  
+  
+  
+  
