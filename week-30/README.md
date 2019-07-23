4630d556faf1c7d26d92a5efb23d48d5219cf7ca
Georgios Karamanis <>
2019-07-23 07:10:21 +0200

    new week

---
+++ week-30/README.md
@@ -0,0 +1,3 @@
+https://github.com/rfordatascience/tidytuesday/tree/master/data/2019/2019-07-23
+
+![](/week-30/wip-sorry.jpg)
---
+++ week-30/w30.R
@@ -0,0 +1,5 @@
+ggsave
+filename = "figure%03d.png"
+
+
+For commercial and GA aircraft, 71 and 73 percent of bird strikes, respectively, occurred at or below 500 feet AGL. Above 500 feet AGL, the number of strikes declined by 34 percent for each 1,000-foot gain in height for commercial aircraft, and by 44 percent for GA aircraft. Strikes occurring above 500 feet were more likely to cause damage than strikes at or below 500 feet. The record height for a reported bird strike was 31,300 feet.
