eb966d7f33621d00f46dae319ba689d6a109d985
Georgios Karamanis <georgios.karamanis@me.com>
2019-08-03 14:39:11 +0300

    New week

---
+++ week-32/README.md
@@ -0,0 +1,3 @@
+https://github.com/rfordatascience/tidytuesday/tree/master/data/2019/2019-08-06
+
+![](wip_sorry.jpg)
---
+++ week-32/w32.R
@@ -0,0 +1,15 @@
+library(tidyverse)
+library(here)
+
+   
+  theme_void(base_size, base_family) +
+  
+  
+  ggsave(here("week-32", "img", paste0("w32", format(Sys.time(), "%Y%m%d_%H%M%S"), ".png")), width = 8, height = 6, dpi = 320)
+  
+
+# https://medium.com/nightingale/the-process-of-familiarity-an-interview-with-nicholas-rougeux-c30f1a1b2f8?source=rss----356ca48206e6---4
+
+
+
+
