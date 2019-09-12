58c05aa60e8397676f9221a248e27c21e55af9fb
Georgios Karamanis <georgios.karamanis@me.com>
2019-09-12 16:34:35 +0200

    Add comments

--- week-37/parks-map.R
+++ week-37/parks-map.R
@@ -4,43 +4,46 @@ library(urbnmapr)
 library(ggsci)
 library(ggsflabel)
 
-
+# read in data
 safer_parks <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2019/2019-09-10/saferparks.csv")
 
+# rename things, keep only two columns
 acc_state <- safer_parks %>%
   rename(state_abbv = acc_state) %>%
   group_by(state_abbv) %>%
   top_n(n = 1, wt = device_category) %>% 
   distinct(state_abbv, device_category) %>% 
-  mutate(
-    device_category = str_replace_all(device_category,
+  mutate(device_category = str_replace_all(device_category,
                                       c("other.+" = "other",
                                       " " = "\n")))
-                                  # c("w/" = "with ",
-                                  #   "- undefined" = "",
-                                  #   "Ferris/gondola" = "Ferris",
-                                  #   "Kamikaze.+" = "Ranger")))
 
+# read map and merge with data
 states_sf <- get_urbn_map("states", sf = TRUE)
 spatial_data <- left_join(states_sf, acc_state, by = "state_abbv")
 
-# get types and move "other" to end of list
+# custom legend order: get types from data, then move "other" to end of list
 x <- unique(sort(acc_state$device_category))
 legend_items <- c(x[-2], x[2])
 
+# plot
 ggplot() +
   geom_sf(data = spatial_data, aes(fill = device_category),
           color = "grey30", size = 0.25) +
+  # labels
   geom_sf_text_repel(data = spatial_data, aes(label = device_category),
                size = 6, family = "IBM Plex Sans Medium", hjust = 0,
                color = "grey15", point.padding = NA, lineheight = 0.9) +
+  # customize legend (label.position puts labels under keys!)
   guides(fill = guide_legend(title.position = "top",
                              title = "Amusement park injuries:\nThe most common device category reported by state", label.hjust = 0,
                              title.hjust = 0, nrow = 1,
                              label.position = "bottom")) +
+  # palette, make NA grey80
   scale_fill_simpsons( na.value = "grey80",
                 limits = legend_items) +
+  # caption
   labs(caption = "Source: data.world & Safer Parks | Graphic by Georgios Karamanis") +
+  # theme
   theme_void(base_family = "IBM Plex Sans", base_size = 20) +
   theme(
     plot.margin = margin(20, 30, 20, 30),
@@ -51,8 +54,8 @@ ggplot() +
     legend.key.width = unit(35, "points"),
     legend.key.height = unit(7, "points")
   ) +
-
+  # save image
   ggsave(
   here::here("week-37", "figures", "temp", paste0("parks_", format(Sys.time(), "%Y%m%d_%H%M%S"), ".png")),
   height = 12, width = 16, dpi = 320
-)
+  )
