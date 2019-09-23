63460170d7dd3e5b2a88f89a01b3959dea84211b
Georgios Karamanis <>
2019-09-23 22:55:06 +0200

    Make new plot

--- week-38/national-parks-mean-pct.R
+++ week-38/national-parks-mean-pct.R
@@ -1,7 +1,8 @@
 library(tidyverse)
 library(here)
 library(lemon)
-library(RColorBrewer)
+library(viridis)
+library(forcats)
 library(scales)
 
 park_visits <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2019/2019-09-17/national_parks.csv")
@@ -11,34 +12,24 @@ pv_ch <- park_visits %>%
   filter(unit_type == "National Park" & year != "Total") %>%
   mutate(year = as.numeric(year)) %>% 
   group_by(unit_name) %>% 
-  arrange(year, .by_group = TRUE) %>%
-  mutate(pct_change = (visitors/lag(visitors) - 1) * 100) %>% 
   filter(unit_name != "Denali National Preserve") %>% 
-  drop_na() %>% 
   mutate(
-    pct_change = case_when(
-      pct_change > 100 ~ 200,
-      pct_change < -100 ~ -200,
-      TRUE ~ pct_change
-    )
-  )
+    mean_visitors = mean(visitors),
+    mean_pct = visitors/mean_visitors
+    ) %>% 
+  add_tally()
 
 
-pv_ch %>%
-  ggplot() +
+ggplot(pv_ch) +
   geom_segment(aes(x = year, xend = year,
-                   y = 0, yend = 1, color = pct_change), size = 1) +
+                   y = 0, yend = 0.5, color = mean_pct), size = 1.4) +
   scale_x_continuous(breaks = seq(1910, 2010, 20), expand = expand_scale(add = c(5, 1))) +
   scale_y_continuous(expand = c(0.05, 0.25)) +
-  facet_wrap(vars(unit_name), ncol = 3) +
-  scale_colour_gradientn(limits = c(-200, 200),
-                         colors = brewer.pal(n = 7, name = "RdYlGn"),
-                         values = rescale(c(-200, -100, -1, 0, 1, 100, 200)),
-                         labels = c("", "-100%", "0", "100%", "")
-  ) +
+  facet_wrap(vars(fct_reorder(unit_name, -n)), ncol = 3) +
+  scale_color_viridis(option = "plasma", labels = percent) +
   labs(
-    title = "National Park Visits 1904–2016",
-    subtitle = "Year-over-year percentage change",
+    title = "National Park Visits, 1904–2016",
+    subtitle = "Percentage of all-time average number of visits, by year",
     caption = "Source: dataisplural/data.world | Graphic: Georgios Karamanis"
   ) +
   guides(color = guide_colorbar(
@@ -47,7 +38,7 @@ pv_ch %>%
     title = NULL,
     barwidth = 20,
     barheight = 0.5
-  )) +
+    )) +
   theme_void(base_family = "IBM Plex Sans") +
   theme(
     legend.position = "top",
@@ -56,7 +47,7 @@ pv_ch %>%
     plot.background = element_rect(fill = "grey80", color = NA),
     strip.background = element_rect(fill = "grey80", color = NA),
     strip.text = element_text(family = "IBM Plex Sans Bold", color = "grey30",
-                              hjust = 1, vjust = 1),
+                               hjust = 1, vjust = 1),
     plot.title = element_text(size = 28, color = "grey20", family = "IBM Plex Sans Medium"),
     plot.subtitle = element_text(size = 20, color = "grey20"),
     plot.caption = element_text(size = 8, color = "grey30", margin = margin(20, 0, 0, 0)),
@@ -64,9 +55,10 @@ pv_ch %>%
     panel.grid.major.x = element_line(color = "grey75"),
     plot.margin = margin(20, 20, 20, 20)
   ) +
-  
-  # save image
+
+# save image
   ggsave(
-    here::here("week-38", "figures", "temp", paste0("national-parks", format(Sys.time(), "%Y%m%d_%H%M%S"), ".png")),
-    width = 18, height = 14, dpi = 320
-  )
+  here::here("week-38", "figures", "temp", paste0("national-parks", format(Sys.time(), "%Y%m%d_%H%M%S"), ".png")),
+  width = 18, height = 14, dpi = 320
+)
+ 
