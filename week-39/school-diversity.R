a81caeab61a6aaf4f2b7e9b4f07792bb25883053
gkaramanis <georgios.karamanis@me.com>
2019-09-26 13:27:02 +0000

    Fill squares, median by state

--- week-39/school-diversity.R
+++ week-39/school-diversity.R
@@ -5,18 +5,28 @@ library(here)
 school_diversity <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2019/2019-09-24/school_diversity.csv")
 
 clean_school <- school_diversity %>% 
-  clean_names() 
+  clean_names() %>% 
+  group_by(st, school_year) %>% 
+  mutate(
+    white = median(white),
+    black = median(black),
+    asian = median(asian),
+    hispanic = median(hispanic),
+    multi = median(multi)
+  ) %>% 
+  distinct(st, white, black, asian, hispanic, multi) %>% 
+  filter(school_year == "2016-2017")
 
-alf = 0.1
 
-ggplot(clean_school) +
-  geom_rect(aes(xmin = -0.1, ymin = 0.1, xmax = -0.1 - white, ymax = 0 + white), alpha = alf) +
-  geom_rect(aes(xmin = 0.1, ymin = 0.1, xmax = 0.1 + black, ymax = 0.1 + black), alpha = alf) +
-  geom_rect(aes(xmin = 0.1, ymin = -0.1, xmax = 0.1 + asian, ymax = -0.1 - asian), alpha = alf) +
-  geom_rect(aes(xmin = -0.1, ymin = -0.1, xmax = -0.1 - hispanic, ymax = -0.1 - hispanic), alpha = alf) +
-  coord_fixed() +
-  facet_wrap(vars(school_year)) +
-
-    ggsave(
+ggplot(clean_school, alpha = 1, size = 0.1) +
+  geom_rect(aes(xmin = -10, ymin = 10, xmax = -10 - white, ymax = 0 + white), fill =  "red") +
+  geom_rect(aes(xmin = 10, ymin = 10, xmax = 10 + black, ymax = 10 + black), fill =  "green") +
+  geom_rect(aes(xmin = 10, ymin = -10, xmax = 10 + asian, ymax = -10 - asian), fill =  "blue") +
+  geom_rect(aes(xmin = -10, ymin = -10, xmax = -10 - hispanic, ymax = -10 - hispanic), fill =  "purple") +
+  geom_tile(aes(x = 0, y = 0, width = multi, height = multi), fill =  "brown") +
+  coord_fixed(xlim = c(-110, 110), ylim = c(-110, 110)) +
+  facet_wrap(vars(st)) +
+  theme_void() +
+  ggsave(
     here::here("week-39", "figures", "temp", paste0("school-diversity", format(Sys.time(), "%Y%m%d_%H%M%S"), ".png"))
     )
