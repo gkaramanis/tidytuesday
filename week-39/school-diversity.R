99964d042a90f82a4e17b7ed86445638308f8997
Georgios Karamanis <>
2019-09-26 13:19:56 +0200

    Squares plot

--- week-39/school-diversity.R
+++ week-39/school-diversity.R
@@ -1,17 +1,22 @@
 library(tidyverse)
 library(janitor)
 library(here)
-library(ggforce)
 
 school_diversity <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2019/2019-09-24/school_diversity.csv")
 
-tri <- school_diversity %>% 
-  clean_names() %>%
-  head(1)
+clean_school <- school_diversity %>% 
+  clean_names() 
 
-ggplot(tri) +
-  geom_rect(aes(x = )) +
-  
+alf = 0.1
+
+ggplot(clean_school) +
+  geom_rect(aes(xmin = -0.1, ymin = 0.1, xmax = -0.1 - white, ymax = 0 + white), alpha = alf) +
+  geom_rect(aes(xmin = 0.1, ymin = 0.1, xmax = 0.1 + black, ymax = 0.1 + black), alpha = alf) +
+  geom_rect(aes(xmin = 0.1, ymin = -0.1, xmax = 0.1 + asian, ymax = -0.1 - asian), alpha = alf) +
+  geom_rect(aes(xmin = -0.1, ymin = -0.1, xmax = -0.1 - hispanic, ymax = -0.1 - hispanic), alpha = alf) +
+  coord_fixed() +
+  facet_wrap(vars(school_year)) +
+
     ggsave(
     here::here("week-39", "figures", "temp", paste0("school-diversity", format(Sys.time(), "%Y%m%d_%H%M%S"), ".png"))
     )
