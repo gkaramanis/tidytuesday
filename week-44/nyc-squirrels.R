058037dd3247a06f61d6e44abc1c784b4c3ff16b
Georgios Karamanis <>
2019-11-01 22:17:33 +0100

    RIP squirrels

--- week-44/nyc-squirrels.R
+++ week-44/nyc-squirrels.R
@@ -1,26 +1,52 @@
 library(tidyverse)
 library(here)
+library(sf)
+library(ggimage)
+library(shadowtext)
+library(ggforce)
 
 nyc_squirrels <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2019/2019-10-29/nyc_squirrels.csv")
 
-squirrels_height <- nyc_squirrels %>% 
+central_park <- read_sf(dsn = here::here("week-44", "data", "central-park", "CentralPark.shp"), layer = "CentralPark")
+
+sq <- nyc_squirrels %>% 
   mutate(
-    height = case_when(
-      above_ground_sighter_measurement == FALSE ~ 0,
-      TRUE ~ as.numeric(above_ground_sighter_measurement)
+    icon = case_when(
+      str_detect(other_activities, "dead") ~ "tombstone"
+      # str_detect(other_activities, "dog") ~ "dog",
+      # str_detect(other_activities, "canoodling") ~ "balloon",
+      # str_detect(other_activities, "upside down") ~ "sloth"
     )
   ) %>% 
-  select(long, lat, height) %>% 
-  filter(!is.na(height))
+  filter(!is.na(icon))
 
-ggplot(squirrels_height, aes(x = long, y = height, color = factor(height))) +
-  geom_point() +
-  scale_colour_viridis_d() +
-  scale_fill_viridis_d() +
+ggplot(sq) +
+  geom_sf(data = central_park, color = "black") +
+  geom_image(
+    aes(long, lat,
+        image = paste0(here::here("week-44", "data", "icons"), "/", icon, ".png")),
+    size = 0.04, asp = 1) +
+  geom_shadowtext(aes(long , lat - 0.0015, label = unique_squirrel_id),
+            color = "grey80", family = "IBM Plex Sans Medium", size = 3, hjust = 0.9) +
+  geom_label(data = data.frame(),
+             aes(-73.948, 40.771,
+                 label = "IN LOVING MEMORY\nOF\n\n39G-AM-1018-01\nAND\n21F-AM-1018-03\n\nYOU ARE FOREVER\nIN OUR HEARTS"),
+             family = "IBM Plex Serif Medium",
+             label.padding = unit(0.75, "lines"),
+             label.size = 0,
+             label.r = unit(0.45, "lines"),
+             alpha = 0.7) +
+  labs(caption = "Source: NYC Squirrel Census | Graphic: Georgios Karamanis") +
+  scale_x_continuous(limits = c(-73.99, -73.94)) +
+  scale_y_continuous(limits = c(40.7644, 40.8022)) +
   theme_void() +
-  theme(legend.position = "none")
-
+  theme(
+    panel.background = element_rect(fill = "darkslateblue", colour = NA),
+    plot.background = element_rect(fill = NA, color = "darkslateblue"),
+    plot.margin = margin(10, 10, 0, 10),
+    plot.caption = element_text(family = "IBM Plex Sans", size = 7, color = "darkslateblue", vjust = 2.5)
+  ) +
   ggsave(
-    here::here("week-44", "plots", "temp", paste0("nyc-squirrels-", format(Sys.time(), "%Y%m%d_%H%M%S"), ".png"))
+    here::here("week-44", "plots", "temp", paste0("nyc-squirrels-", format(Sys.time(), "%Y%m%d_%H%M%S"), ".png")), dpi = 320, height = 7, width = 7
     )
 
