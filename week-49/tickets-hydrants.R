95fbe4625ce82fec23ba5dbaf223f8d9c6055174
Georgios <georgios@Georgioss-MacBook-Pro.local>
2019-12-12 21:55:09 +0100

    Cleaning

--- week-49/tickets-hydrants.R
+++ week-49/tickets-hydrants.R
@@ -2,7 +2,6 @@ library(tidyverse)
 library(here)
 library(osmdata)
 
-
 tickets <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2019/2019-12-03/tickets.csv")
 
 philadelphia <- getbb("Philadelphia USA")%>%
