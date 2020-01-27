7aacfc6ccc98fb1f66bce87a4bdc1c1eb1001cc8
Georgios <georgios@Georgioss-MacBook-Pro.local>
2020-01-27 10:25:38 +0100

    New week

--- 2020-week05/sf-trees.R
+++ 2020-week05/sf-trees.R
@@ -2,6 +2,6 @@ library(tidyverse)
 library(here)
 
 
-+ ggsave(here::here("2020-week04", "plots", "temp", paste0("spotify-", format(Sys.time(), "%Y%m%d_%H%M%S"), ".png")), dpi = 320, width = 12, height = 8
++ ggsave(here::here("2020-week05", "plots", "temp", paste0("sf-trees-", format(Sys.time(), "%Y%m%d_%H%M%S"), ".png")), dpi = 320, width = 12, height = 8
 )
 
