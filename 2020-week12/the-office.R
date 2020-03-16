834f430f82c5b97119182d1db96fb84c26a182aa
Georgios <georgios@Georgios-iMac.local>
2020-03-16 18:39:27 +0100

    New week

--- 2020-week12/the-office.R
+++ 2020-week12/the-office.R
@@ -1,6 +1,8 @@
 library(tidyverse)
+# install.packages("schrute")
+library(schrute)
 
-office_ratings <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-03-17/office_ratings.csv')
+theoffice <- theoffice
 
 # ggsave(here::here("2020-week12", "plots", "the-office.png"), dpi = 320)
     
