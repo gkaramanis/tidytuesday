619872c317824a42cf2b87ef9b9c395349dac2e7
Georgios <georgios@Georgioss-MacBook-Pro.local>
2020-09-19 05:48:47 +0200

    Table is ready

--- 2020-week38/README.md
+++ 2020-week38/README.md
@@ -1,3 +1,4 @@
 https://github.com/rfordatascience/tidytuesday/tree/master/data/2020/2020-09-15
 
-![](plots/kids.png)
+![](plots/kids.png)  
+![](plots/kids-table.png)
--- 2020-week38/kids-table.R
+++ 2020-week38/kids-table.R
@@ -5,49 +5,100 @@ kids <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidyt
 
 libraries <- kids %>% 
   filter(variable == "lib") %>% 
-  mutate(inf_adj_perchild = round(inf_adj_perchild * 1000)) 
+  select(state, year, inf_adj_perchild) %>% 
+  mutate(inf_adj_perchild = round(inf_adj_perchild * 1000))
 
-plot_spark <- function(data){
-  data %>% 
-    ggplot(aes(x = year, y = inf_adj_perchild)) +
+plot_group <- function(name, df){
+  plot_object <- df %>% 
+    mutate(
+      start_inf = if_else(year == 1997, inf_adj_perchild, NA_real_),
+      end_inf = if_else(year == 2016, inf_adj_perchild, NA_real_)
+    ) %>% 
+    tidyr::fill(start_inf, end_inf, .direction = "downup") %>% 
+    mutate(color = if_else(end_inf - start_inf < 0, "red", "blue")) %>% 
+    ggplot(aes(x = year, y = inf_adj_perchild,color = color)) +
     geom_line(size = 15) +
-    theme_void()
+    theme_void() +
+    scale_color_identity() +
+    theme(legend.position = "none")
+  
+  return(plot_object)
 }
 
-libraries_plot <- libraries %>% 
-  select(state, year, inf_adj_perchild) %>% 
-  nest(spent = c(year, inf_adj_perchild)) %>% 
-  mutate(plot = map(spent, plot_spark))
+sparklines <- libraries %>% 
+  group_by(state) %>% 
+  nest() %>% 
+  mutate(plot = map2(state, data, plot_group)) %>% 
+  select(-data)
 
-
-libraries %>% 
+table_prepped <- libraries %>% 
   filter(year == 1997 | year == 2016) %>%
   pivot_wider(id_cols = state, names_from = year, values_from = inf_adj_perchild) %>% 
-  mutate(change = round((`2016` - `1997` )/ `1997` * 100)) %>% 
+  mutate(change = round((`2016` - `1997` ) / `1997` * 100)) %>%
+  inner_join(sparklines, by = "state") %>% 
+  mutate(ggplot = NA)
+
+top_55 <- table_prepped %>% 
   filter(dense_rank(change) <= 5 | dense_rank(desc(change)) <= 5) %>% 
-  arrange(desc(change)) %>% 
-  mutate(ggplot = NA) %>% 
+  arrange(-change)
+  
+top_55 %>% 
   gt() %>% 
   text_transform(
     locations = cells_body(vars(ggplot)),
     fn = function(x){
-      map(libraries_plot$plot, ggplot_image, height = px(15), aspect_ratio = 4)
+      map(top_55$plot, ggplot_image, height = px(15), aspect_ratio = 4)
     }
-  ) %>% 
+  ) %>%
+  tab_row_group(
+    group = "Bottom 5",
+    rows = 6:10
+  ) %>%
+  tab_row_group(
+    group = "Top 5",
+    rows = 1:5
+  ) %>%
   cols_label(
-    ggplot = "1997-2016"
+    ggplot = "1997-2016",
+    state = "State",
+    change = "% Change"
   ) %>%  
-  tab_options(
-    row_group.border.top.width = px(3),
-    row_group.border.top.color = "black",
-    row_group.border.bottom.color = "black",
-    table_body.hlines.color = "white",
-    table.border.top.color = "white",
-    table.border.top.width = px(3),
-    table.border.bottom.color = "white",
-    table.border.bottom.width = px(3),
-    column_labels.border.bottom.color = "black",
-    column_labels.border.bottom.width = px(2),
+  tab_style(
+    style = cell_text(font = "Proxima Nova"),
+    locations = cells_body(columns = 1)
   ) %>% 
-  tab_source_note("Source: Urban Institute | Table: Georgios Karamanis")
-  
+  tab_style(
+    style = cell_text(font = "IBM Plex Sans Condensed"),
+    locations = cells_body(columns = 2:4)
+  ) %>% 
+  tab_style(
+    style = cell_text(font = "Graphik Compact", weight = "bold"),
+    locations = cells_column_labels(columns = gt::everything())
+  ) %>% 
+  tab_style(
+    style = cell_text(font = "Graphik Compact"),
+    locations = cells_title()
+  ) %>% 
+  tab_style(
+    style = cell_text(font = "Graphik Compact"),
+    locations = cells_row_groups()
+  ) %>% 
+  tab_header(
+    title = "Change in public spending on libraries",
+    subtitle = "Dollars spent per child, adjusted for inflation"
+  ) %>% 
+  tab_source_note("Source: Urban Institute, Table: Georgios Karamanis") %>% 
+  tab_options(
+    row_group.border.top.width = px(5),
+    row_group.border.top.color = "white",
+    row_group.border.bottom.width = px(1),
+    row_group.border.bottom.color = "lightgrey",
+    table.border.top.color = "white",
+    table.border.top.width = px(5),
+    table.border.bottom.color = "white",
+    column_labels.border.bottom.color = "white",
+    column_labels.border.bottom.width = px(2),
+    column_labels.border.top.width = px(10),
+    column_labels.border.top.color = "white"
+  ) %>% 
+  cols_hide(vars(plot))
