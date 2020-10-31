d93f317cb7aa19ffd6e50192931afa4bad72baac
Georgios <georgios.karamanis@me.com>
2020-10-31 13:29:38 +0100

    Fix rotation direction

--- 2020-week44/wind-turbine-animation.R
+++ 2020-week44/wind-turbine-animation.R
@@ -26,9 +26,9 @@ w_turb <- wind_turbine %>%
 
 anim <- ggplot(w_turb) +
   geom_segment(aes(x = province_n * 200, y = 0, xend = province_n * 200, yend = hub_height_m), stat = "unique") +
-  geom_spoke(aes(x = province_n * 200, y = hub_height_m, angle = log(n) * t + blade1, radius = rotor_r), stat = "unique") +
-  geom_spoke(aes(x = province_n * 200, y = hub_height_m, angle = log(n) * t + blade2, radius = rotor_r), stat = "unique") +
-  geom_spoke(aes(x = province_n * 200, y = hub_height_m, angle = log(n) * t + blade3, radius = rotor_r), stat = "unique") +
+  geom_spoke(aes(x = province_n * 200, y = hub_height_m, angle = -log(n) * t + blade1, radius = rotor_r), stat = "unique") +
+  geom_spoke(aes(x = province_n * 200, y = hub_height_m, angle = -log(n) * t + blade2, radius = rotor_r), stat = "unique") +
+  geom_spoke(aes(x = province_n * 200, y = hub_height_m, angle = -log(n) * t + blade3, radius = rotor_r), stat = "unique") +
   geom_text(aes(x = province_n * 200, y = hub_height_m + 90, label = n), stat = "unique", family = "Atkinson Hyperlegible", size = 8) +
   geom_text(aes(x = province_n * 200, y = -20, label = str_wrap(province_territory, 10)), stat = "unique", vjust = 1, lineheight = 0.9, family = "Atkinson Hyperlegible Bold", size = 6) +
   coord_fixed(clip = "off") +
