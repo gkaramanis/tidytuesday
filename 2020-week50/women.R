21feb8e138c203df8cd70e8b76dbd6cbb43379d1
Georgios <georgios.karamanis@me.com>
2020-12-08 05:38:20 +0100

    start new week

---
+++ 2020-week50/women.R
@@ -0,0 +1,4 @@
+library(tidyverse)
+
+ggsave(here::here("temp", paste0("women-", format(Sys.time(), "%Y%m%d_%H%M%S"), ".png")), dpi = 320)
+
--- script-of-the-week.R
+++ script-of-the-week.R
@@ -1,1 +1,1 @@
-eval(parse(text="source(\"./2020-week46/phone.R\")"))
+eval(parse(text="source(\"./2020-week50/women.R\")"))
