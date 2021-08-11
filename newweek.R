dba1f3bdfad2ed4d162da5ab170328eef1599522
Georgios Karamanis <georgios.karamanis@me.com>
2021-08-11 05:56:32 +0200

    add newline

--- newweek.R
+++ newweek.R
@@ -33,6 +33,7 @@ script_text <- paste0(
   # gg_playback(frame_duration = 0.15, image_resize = 1080)
   # convert to mp4 in terminal
   # ffmpeg -i 2021_08_01_14_53_40.gif -movflags faststart -pix_fmt yuv420p -vf "scale=trunc(iw/2)*2:trunc(ih/2)*2" olympics_makingof.mp4',
+  '\n\n',
   '# ggsave(here::here(',
   '\"temp\", paste0(\"',
   newscript,
