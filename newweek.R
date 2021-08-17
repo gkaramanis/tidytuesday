d100f79b575f87f60764a71f0cfb80fea37f1858
Georgios Karamanis <georgios.karamanis@me.com>
2021-08-17 06:34:18 +0200

    update script

--- newweek.R
+++ newweek.R
@@ -32,7 +32,7 @@ script_text <- paste0(
   '# export gif
   # gg_playback(frame_duration = 0.15, image_resize = 1080)
   # convert to mp4 in terminal
-  # ffmpeg -i 2021_08_01_14_53_40.gif -movflags faststart -pix_fmt yuv420p -vf "scale=trunc(iw/2)*2:trunc(ih/2)*2" olympics_makingof.mp4',
+  # ffmpeg -i animated.gif -movflags faststart -pix_fmt yuv420p -vf "scale=trunc(iw/2)*2:trunc(ih/2)*2" makingof.mp4',
   '\n\n',
   '# ggsave(here::here(',
   '\"temp\", paste0(\"',
