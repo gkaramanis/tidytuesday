dac7bb98bf8e2ba7371ece5c1a9d9734221175db
Georgios Karamanis <georgios.karamanis@me.com>
2021-11-06 17:35:39 +0100

    put France back

--- 2021/2021-week44/map.R
+++ 2021/2021-week44/map.R
@@ -14,23 +14,22 @@ ortho <- paste0('+proj=ortho +lat_0=', lat, ' +lon_0=', lon,
                 ' +x_0=0 +y_0=0 +a=6371000 +b=6371000 +units=m +no_defs')
 
 circle <- st_point(x = c(0, 0)) %>%
-      st_buffer(dist = 6371000) %>%
-      st_sfc(crs = ortho) 
-
-circle_longlat <- circle %>%
+  st_buffer(dist = 6371000) %>%
+  st_sfc(crs = ortho) %>%
   st_transform(crs = 4326)
 
-world_ortho <- world %>%
+world_ortho <- st_cast(world, 'MULTIPOLYGON') %>%
+  st_cast('POLYGON', do_split = TRUE) %>%
   st_transform(crs = ortho)
 
 f1 = "Porpora"
 
 ggplot(world_ortho) +
-  # with_blur(
-  #   geom_sf(data = circle, fill = "cornflowerblue", color = NA, size = 2),
-  #   sigma = unit(3, 'mm')
-  # ) + 
-  # geom_sf(data = circle, fill = "#06216E", color = "black", size = 0.3) +
+  with_blur(
+    geom_sf(data = circle, fill = "cornflowerblue", color = NA, size = 2),
+    sigma = unit(3, 'mm')
+  ) +
+  geom_sf(data = circle, fill = "#06216E", color = "black", size = 0.3) +
   geom_sf(aes(fill = lifeExp), size = 0.2, color = "#06216E") +
   scale_fill_fermenter(palette = "Purples", na.value = "#8c7d70") +
   labs(
