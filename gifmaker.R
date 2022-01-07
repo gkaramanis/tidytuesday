e556a0f302016f41ef917fb1545bd6adf6f7cdb4
Georgios <georgios@Georgios-MacBook-Pro-M1.local>
2022-01-07 10:31:42 +0100

    change path to ffmpeg

--- gifmaker.R
+++ gifmaker.R
@@ -5,4 +5,4 @@ gg_record(dir = "temp")
 gg_playback(frame_duration = 0.15, image_resize = 1080, name = "temp/animated.gif", playback = FALSE)
 
 # convert to mp4
-system('ffmpeg -i ./temp/animated.gif -movflags faststart -pix_fmt yuv420p -vf "scale=trunc(iw/2)*2:trunc(ih/2)*2" ./temp/making-of.mp4')
+system('/opt/homebrew/bin//ffmpeg -i ./temp/animated.gif -movflags faststart -pix_fmt yuv420p -vf "scale=trunc(iw/2)*2:trunc(ih/2)*2" ./temp/making-of.mp4')
