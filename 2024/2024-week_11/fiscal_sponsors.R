fc362129757a14119ce2d1c099c231efe597d2a6
Georgios Karamanis <georgios@karaman.is>
2024-03-11 18:06:10 +0100

    fix the mess

--- 2024/2024-week_11/fiscal_sponsors.R
+++ 2024/2024-week_11/fiscal_sponsors.R
@@ -6,4 +6,8 @@ gg_record(dir = "tidytuesday-temp", device = "png", width = 10, height = 8, unit
 fiscal_sponsor_directory <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2024/2024-03-12/fiscal_sponsor_directory.csv')
 
 lgbtq_fiscal <- fiscal_sponsor_directory %>% 
-  filter(str_detect(eligibility_criteria, "LGBTQ"))
+  filter(str_detect(eligibility_criteria, "LGBTQ") | str_detect(project_types, "LGBTQ"))
+
+ggplot(lgbtq_fiscal) +
+  geom_point(aes(year_501c3, year_fiscal_sponsor, size = n_sponsored), shape = 21) +
+  scale_size_area(max_size = 20)
