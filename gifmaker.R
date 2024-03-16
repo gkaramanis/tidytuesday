14ce0cba58a1630bef2b86b2a0d3c25dcd7d9deb
Georgios Karamanis <georgios@karaman.is>
2024-03-16 17:34:23 +0100

    remove empty lines

--- gifmaker.R
+++ gifmaker.R
@@ -6,5 +6,3 @@ gg_playback(frame_duration = 0.15, image_resize = 1080, name = "tidytuesday-temp
 
 # convert to mp4
 system('/opt/homebrew/bin//ffmpeg -i ./tidytuesday-temp/animated.gif -movflags faststart -pix_fmt yuv420p -vf "scale=trunc(iw/2)*2:trunc(ih/2)*2" -vf tpad=stop_mode=clone:stop_duration=0.1 ./tidytuesday-temp/making-of.mp4')
-
-
