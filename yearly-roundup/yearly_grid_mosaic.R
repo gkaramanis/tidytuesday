a79bc5e6fa5f1b7232b014e4dd63c9e9eb712cf1
Georgios Karamanis <georgios@karaman.is>
2024-03-16 17:34:10 +0100

    add 5th year

--- yearly-roundup/yearly_grid_mosaic.R
+++ yearly-roundup/yearly_grid_mosaic.R
@@ -12,23 +12,26 @@
 
 
 Change year folder!
-  
+
   
 library(dplyr)
 library(stringr)
 
-plots <- list.files(c("2022", "2023"), pattern = "png", recursive = TRUE, full.names = TRUE) %>% 
+# Change years
+plots <- list.files(c("2023", "2024"), pattern = "png", recursive = TRUE, full.names = TRUE) %>% 
   as_tibble() %>% 
   rename("from" = "value") %>% 
   filter(str_detect(from, "plots")) %>% 
-  mutate(to = paste0("yearly-roundup/fourth-year/", str_replace_all(from, "/", "_")))
+  # Change year
+  mutate(to = paste0("yearly-roundup/fifth-year/", str_replace_all(from, "/", "_")))
 
 file.copy(plots$from, plots$to)
 
-dir.create("yearly-roundup/fourth-year/resized/")
+dir.create("yearly-roundup/fifth-year/resized/", recursive = TRUE)
 
-# Remove unneeded images before next step
+# Manually remove unneeded images before next step
+# Change year
+system('sips -Z 800 yearly-roundup/fifth-year/*.png --out \"yearly-roundup/fifth-year/resized/\"')
 
-system('sips -Z 800 yearly-roundup/fourth-year/*.png --out \"yearly-roundup/fourth-year/resized/\"')
-
-system('/opt/homebrew/bin//montage -density 300 -tile 9x0 -geometry 400x400+10+10 -border 0 -background lightgrey yearly-roundup/fourth-year/resized/*.png yearly-roundup/fourth-year/fourth-year.png')
+# Change year
+system('/opt/homebrew/bin//montage -density 300 -tile 9x0 -geometry 400x400+10+10 -border 0 -background lightgrey yearly-roundup/fifth-year/resized/*.png yearly-roundup/fifth-year/fifth-year.png')
