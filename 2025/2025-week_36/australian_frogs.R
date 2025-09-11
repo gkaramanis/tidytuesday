ec7b99e9b09e26ab5fca9b39edb73a6978d4c78c
Georgios Karamanis <georgios.karamanis@me.com>
2025-09-11 06:44:58 +0200

    Stage current state before pull

--- 2025/2025-week_36/australian_frogs.R
+++ 2025/2025-week_36/australian_frogs.R
@@ -14,16 +14,27 @@ frogs <- frogID_data |>
   mutate(
     month = month(eventDate),
     season = case_when(
-      month %in% c(1, 2) ~ "Summer  \n(Jan-Feb)",
-      month %in% c(3, 4, 5) ~ "Autumn  \n(Mar-May)",
-      month %in% c(6, 7, 8) ~ "Winter  \n(Jun-Aug)",
-      month %in% c(9, 10, 11) ~ "Spring  \n(Sep-Nov)",
+      month %in% c(12, 1, 2) ~ "Summer",
+      month %in% c(3, 4, 5) ~ "Autumn",
+      month %in% c(6, 7, 8) ~ "Winter",
+      month %in% c(9, 10, 11) ~ "Spring",
       TRUE ~ NA
     ),
   ) |>
-  mutate(season = fct_inorder(season)) |> 
   add_count(tribe, season) |>
   st_as_sf(coords = c("decimalLongitude", "decimalLatitude"), crs = 4326)
+
+common_frogs <- frogs |> 
+  st_drop_geometry() |> 
+  count(tribe, commonName) |> 
+  group_by(tribe) |>
+  slice_max(order_by = n, n = 3) |> 
+  ungroup() |> 
+  group_by(tribe) |> 
+  reframe(label = paste0("**", tribe, "**  \n", 
+                         str_wrap(str_c(commonName, collapse = ", "), 10)
+                         )) |> 
+  distinct()
 
 hex_grid <- st_make_grid(frogs, square = FALSE, n = c(30, 30))
 hex_sf <- st_sf(hex_id = seq_along(hex_grid), geometry = hex_grid)
@@ -38,6 +49,7 @@ hex_counts <- frogs |>
     x = st_coordinates(centroid)[, 1],
     y = st_coordinates(centroid)[, 2]
   ) |> 
+  left_join(common_frogs) |> 
   `st_geometry<-`("geometry")
 
 aus <- rnaturalearth::ne_countries(country = "Australia")
@@ -45,30 +57,25 @@ aus <- rnaturalearth::ne_countries(country = "Australia")
 f1 <- "Bricolage Grotesque 12pt"
 f2 <- "Bricolage Grotesque 12pt Condensed"
 
-pal <- MetBrewer::met.brewer("Tam")
-
 ggplot() +
   geom_sf(data = aus, color = NA, fill = "grey90") +
   geom_sf(data = hex_counts, aes(fill = n), color = "grey99", linewidth = 0.1) +
   ggrepel::geom_text_repel(data = hex_counts |> filter(n == max(n), .by = c(season, tribe)), aes(geometry = geometry, label = format(n, big.mark = " ")), color = "black", size = 3.5, fontface = "bold", family = f2, stat = "sf_coordinates", bg.color = "white", seed = 99) +
-  scale_fill_gradientn(colors = pal, label = scales::label_number()) +
-  facet_grid(vars(season), vars(tribe)) +
+  MetBrewer::scale_fill_met_c("Tam") +
+  facet_grid(vars(season), vars(label)) +
   labs(
     title = "Australian frog observations by tribe and season",
-    subtitle = "Number of frog observations in FrogID, an Australian frog call identification initiative, in 2023. Labels show the maximum count per tribe and season.",
-    caption = "Source: FrogID · Graphic: Georgios Karamanis",
-    fill = "Number of observations"
+    subtitle = "Number of frog observations recorded in each hexagonal grid cell. Labels show the maximum count per tribe and season.",
+    caption = "Source: FrogID · Graphic: Georgios Karamanis"
   ) + 
   theme_void(base_family = f1) +
   theme(
     legend.position = "bottom",
     legend.key.width = unit(2, "line"),
     legend.key.height = unit(0.3, "line"),
-    legend.title.position = "top",
-    legend.title = element_text(hjust = 0.5, size = 10),
     plot.background = element_rect(fill = "grey99", color = NA),
     panel.background = element_rect(fill = "grey95", color = NA),
-    strip.text = marquee::element_marquee(size = 10, margin = margin(b = 5)),
+    strip.text = ggtext::element_markdown(size = 5, margin = margin(b = 5)),
     plot.title = element_text(face = "bold"),
     plot.subtitle = element_text(margin = margin(t = 5, b = 10)),
     plot.margin = margin(10, 10, 10, 10)
